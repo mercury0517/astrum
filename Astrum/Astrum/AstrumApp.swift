1c45c9ea0335d3ccb6771dba01d2ea63aac20e5b
Akihiro <aki19960517@gmail.com>
2022-10-22 19:31:00 +0900

    Xcodeのプロジェクト作成

---
+++ Astrum/Astrum/AstrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstrumApp.swift
+//  Astrum
+//
+//  Created by 伊原明宏 on 2022/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AstrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
