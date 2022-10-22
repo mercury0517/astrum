3f8e91e8151e8951d458eebc60d30b4db3125c1c
Akihiro <aki19960517@gmail.com>
2022-10-22 19:38:31 +0900

    ガラス風のUIを配置した。

--- Astrum/Astrum/AstrumApp.swift
+++ Astrum/Astrum/AstrumApp.swift
@@ -11,7 +11,12 @@ import SwiftUI
 struct AstrumApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ZStack{
+                Color.blue
+                    .edgesIgnoringSafeArea(.all)
+
+                GlassBackGround()
+            }
         }
     }
 }
---
+++ Astrum/Astrum/GlassBackGround.swift
@@ -0,0 +1,36 @@
+//
+//  GlassBackGround.swift
+//  Astrum
+//
+//  Created by 伊原明宏 on 2022/10/22.
+//
+
+import SwiftUI
+
+struct GlassBackGround: View {
+    var body: some View {
+        ZStack{
+            RadialGradient(colors: [.clear, .white],
+            center: .center,
+            startRadius: 1,
+            endRadius: 100)
+            .opacity(0.6)
+            Rectangle().foregroundColor(.white)
+        }
+        .opacity(0.1)
+        .blur(radius: 1)
+        .cornerRadius(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+        .stroke(.white, lineWidth: 0.5)
+        )
+        .frame(width: UIScreen.main.bounds.width - 32, height: 300)
+        .padding()
+    }
+}
+
+struct GlassBackGround_Previews: PreviewProvider {
+    static var previews: some View {
+        GlassBackGround()
+    }
+}
