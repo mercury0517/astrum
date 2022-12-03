722e0fec555e082f06218fa2bbf676aa5b8ffffc
mercury0517 <aki19960517@gmail.com>
2022-12-03 22:39:41 +0900

    写真をライブラリから追加してディスプレイに表示できる様にした。

--- Astrum/Astrum/DisplayScreenView.swift
+++ Astrum/Astrum/DisplayScreenView.swift
@@ -8,43 +8,69 @@
 import SwiftUI
 
 struct DisplayScreenView: View {
+    @State var image: UIImage?
+    @State var showingAlert: Bool = false
+    
     let itemWidth = UIScreen.main.bounds.width - 32
     
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 15)
-                .fill(
-                    LinearGradient(
-                        gradient: Gradient(colors: [.grayDark, .black]),
-                        startPoint: .topLeading,
-                        endPoint: .bottomTrailing
+        if let image = image {
+            ZStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(.black)
+                    .frame(width: itemWidth - 10, height: 240)
+                Image(uiImage: image)
+                    .resizable()
+                    .frame(width: itemWidth - 20, height: 230)
+                    .cornerRadius(15)
+                    .padding(EdgeInsets(
+                        top: 10,
+                        leading: 10,
+                        bottom: 10,
+                        trailing: 10
+                    ))
+            }
+        } else {
+            ZStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [.grayDark, .black]),
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        )
                     )
-                )
-                .padding(EdgeInsets(
-                    top: 10,
-                    leading: 10,
-                    bottom: 20,
-                    trailing: 10
-                ))
-                .frame(width: itemWidth, height: 250)
-            VStack {
-                Text("Let's start with setting up your desk.")
-                    .foregroundColor(.backLightYellow)
-                    .font(.title)
-                Button(action: {
-                    print("tap buton")
-                }) {
-                    Text("Add your desk")
+                    .padding(EdgeInsets(
+                        top: 10,
+                        leading: 10,
+                        bottom: 10,
+                        trailing: 10
+                    ))
+                    .frame(width: itemWidth, height: 250)
+                VStack {
+                    Text("Let's start with setting up your desk.")
+                        .foregroundColor(.backLightYellow)
+                        .font(.title)
+                    Button(action: {
+                        showingAlert = true
+                    }) {
+                        Text("Add your desk")
+                    }
+                    .padding(EdgeInsets(
+                        top: 5,
+                        leading: 8,
+                        bottom: 5,
+                        trailing: 8
+                    ))
+                    .accentColor(Color.white)
+                    .background(Color.blue)
+                    .cornerRadius(16)
+                    .sheet(isPresented: $showingAlert) {
+                                
+                    } content: {
+                        ImagePicker(image: $image)
+                    }
                 }
-                .padding(EdgeInsets(
-                    top: 5,
-                    leading: 8,
-                    bottom: 5,
-                    trailing: 8
-                ))
-                .accentColor(Color.white)
-                .background(Color.blue)
-                .cornerRadius(16)
             }
         }
     }
