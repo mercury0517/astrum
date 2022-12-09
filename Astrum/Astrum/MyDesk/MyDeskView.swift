3ac8e5613aacdccbf826bb2f58ddb914a2bf97a3
mercury0517 <aki19960517@gmail.com>
2022-12-09 20:39:53 +0900

    MyDeskタブにナビゲーションを追加

--- Astrum/Astrum/MyDesk/MyDeskView.swift
+++ Astrum/Astrum/MyDesk/MyDeskView.swift
@@ -11,44 +11,50 @@ struct MyDeskView: View {
     @State var deskImage: UIImage? = ImageManager.shared.getImage(name: "deskImage")
     
     var body: some View {
-        ZStack{
-            LinearGradient(
-                gradient: Gradient(colors: [.matteBlack, .black]),
-                startPoint: .top,
-                endPoint: .bottom
-            )
-            .ignoresSafeArea()
-            
-            ScrollView(showsIndicators: true) {
-                VStack {
-                    HStack {
-                        Text("My Desk")
-                            .modifier(SectionTitle())
+        NavigationView {
+            ZStack{
+                LinearGradient(
+                    gradient: Gradient(colors: [.matteBlack, .black]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .ignoresSafeArea()
+                
+                ScrollView(showsIndicators: true) {
+                    VStack {
+                        HStack {
+                            Text("My Desk")
+                                .modifier(SectionTitle())
+                            
+                            Spacer()
+                            
+                            Menu {
+                                Button(
+                                    "Delete the desk image",
+                                    role: .destructive,
+                                    action: { resetDeskImage() }
+                                )
+                            } label: {
+                                Image(systemName: "ellipsis")
+                                    .roundButton()
+                            }
+                            .padding(.trailing, 16)
+                        }
+                        .padding(.top, 16)
+                        
+                        DisplayView(deskImage: $deskImage)
+                        
+                        ItemsView()
+                            .padding(.top, 24)
                         
                         Spacer()
-                        
-                        Menu {
-                            Button(
-                                "Delete the desk image",
-                                role: .destructive,
-                                action: { resetDeskImage() }
-                            )
-                        } label: {
-                            Image(systemName: "ellipsis")
-                                .roundButton()
-                        }
-                        .padding(.trailing, 16)
                     }
-                    .padding(.top, 16)
-                    
-                    DisplayView(deskImage: $deskImage)
-                    
-                    ItemsView()
-                        .padding(.top, 24)
-                    
-                    Spacer()
                 }
             }
+            .navigationTitle("My Desk")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarColorScheme(.dark, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
         }
     }
     
