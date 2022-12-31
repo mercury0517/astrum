6626404777b09ab35d838865075ae5bc28cb83f7
mercury0517 <aki19960517@gmail.com>
2022-12-31 15:09:32 +0900

    デスクトップ部分をカラフルにした

--- Astrum/Astrum/MyDesk/MyDeskView.swift
+++ Astrum/Astrum/MyDesk/MyDeskView.swift
@@ -15,6 +15,9 @@ struct MyDeskView: View {
         
         let navigationBarAppearance = UINavigationBarAppearance()
         navigationBarAppearance.configureWithDefaultBackground()
+        
+        navigationBarAppearance.backgroundColor = UIColor(40, 40, 43)
+        navigationBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
         UINavigationBar.appearance().standardAppearance = navigationBarAppearance
         UINavigationBar.appearance().compactAppearance = navigationBarAppearance
         UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
@@ -27,8 +30,18 @@ struct MyDeskView: View {
                 
                 ScrollView(showsIndicators: true) {
                     VStack {
-                        DisplayView(deskImage: $deskImage)
-                            .padding(.top, 24)
+                        ZStack {
+                            LinearGradient(
+                                gradient: Gradient(colors: [.orange, .red, .purple, .blue]),
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                            .frame(width: UIScreen.main.bounds.width, height: 350)
+                            
+                            DisplayView(deskImage: $deskImage)
+                                .padding(.top, 16)
+                        }
+                        
                         
                         ItemView()
                             .padding(.top, 24)
