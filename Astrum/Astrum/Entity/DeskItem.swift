c410f10bae430a18c528acd4101a7b7074035e9b
mercury0517 <aki19960517@gmail.com>
2023-01-07 13:06:00 +0900

    Realmの導入とアイテム用のEntity作成

---
+++ Astrum/Astrum/Entity/DeskItem.swift
@@ -0,0 +1,16 @@
+//
+//  DeskItem.swift
+//  Astrum
+//
+//  Created by 伊原明宏 on 2023/01/07.
+//
+
+import Foundation
+
+struct DeskItem {
+    let id: String
+    let title: String
+    let memo: String?
+    let url: String?
+    var isWishList: Bool
+}
