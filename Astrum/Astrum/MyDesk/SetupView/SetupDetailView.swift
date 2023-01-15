f0fe301e17fa174ea24ede6633fe848a9d552ac6
mercury0517 <aki19960517@gmail.com>
2023-01-15 12:22:55 +0900

    セットアップにタイトルをつけられる様にした

--- Astrum/Astrum/MyDesk/SetupView/SetupDetailView.swift
+++ Astrum/Astrum/MyDesk/SetupView/SetupDetailView.swift
@@ -11,11 +11,14 @@ import RealmSwift
 struct SetupDetailView: View {
     let itemColor: Color
     @State private var items: [DeskItem]
+    @State private var title: String
     @State private var isAddSetupViewPresented = false
+    @State private var showingTitleAlert = false
     @State private var showingAlert = false
 
     init(itemColor: Color) {
         self.itemColor = itemColor
+        self.title = UserDefaults.standard.object(forKey: "setup_title_\(self.itemColor)") as? String ?? "タイトル"
 
         if let itemIDList = UserDefaults.standard.array(forKey: "setups_\(self.itemColor)") as? [String] {
             let realm = try! Realm()
@@ -40,6 +43,31 @@ struct SetupDetailView: View {
             BackgroundView()
 
             ScrollView {
+                HStack {
+                    Text(title)
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                    Button(action: {
+                        showingTitleAlert = true
+                    }) {
+                        Image(systemName: "pencil.circle")
+                            .roundButton()
+                            .foregroundColor(.blue)
+                            .padding(.leading, 4)
+                    }
+                    .alert("タイトルを入力", isPresented: $showingTitleAlert) {
+                        TextField("", text: $title)
+                        
+                        Button("キャンセル", action: {})
+                        Button("OK", action: { UserDefaults.standard.set(title, forKey: "setup_title_\(self.itemColor)") })
+                    }
+
+                    Spacer()
+                }
+                .padding(.bottom, 16)
+
                 // アイテムがない時はこちらを表示する
                 if items.isEmpty {
                     VStack(alignment: .leading, spacing: 16) {
@@ -73,6 +101,11 @@ struct SetupDetailView: View {
                     ToolbarItem(placement: .navigationBarTrailing) {
                         Menu {
                             Button(
+                                "セットアップを編集",
+                                role: .none,
+                                action: { isAddSetupViewPresented = true }
+                            )
+                            Button(
                                 "セットアップを削除",
                                 role: .destructive,
                                 action: { showingAlert = true }
@@ -90,12 +123,18 @@ struct SetupDetailView: View {
                 Button("削除", role: .destructive) {
                     // キャッシュと現在表示している画面の更新
                     UserDefaults.standard.removeObject(forKey: "setups_\(self.itemColor)")
+                    UserDefaults.standard.removeObject(forKey: "setup_title_\(self.itemColor)")
                     items = []
 
                     HapticFeedbackManager.shared.play(.impact(.soft))
                 }
             }
+            .sheet(isPresented: $isAddSetupViewPresented) {
+                SetupSelectItemView(itemColor: itemColor, setupItems: $items)
+            }
         }.onAppear {
+            self.title = UserDefaults.standard.object(forKey: "setup_title_\(self.itemColor)") as? String ?? "タイトル"
+
             if let itemIDList = UserDefaults.standard.array(forKey: "setups_\(self.itemColor)") as? [String] {
                 let realm = try! Realm()
                 let cachedItemList = Array(realm.objects(DeskItem.self))
