9b63a06a947bce3ff0108de1121fe9f92364b88e
mercury0517 <aki19960517@gmail.com>
2023-01-15 11:16:34 +0900

    セットアップを登録して一覧に表示して、削除もできる様にした

--- Astrum/Astrum/MyDesk/SetupView/SetupDetailView.swift
+++ Astrum/Astrum/MyDesk/SetupView/SetupDetailView.swift
@@ -6,15 +6,33 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct SetupDetailView: View {
     let itemColor: Color
-    @State private var isAddSetupViewPresented = false
     @State private var items: [DeskItem]
+    @State private var isAddSetupViewPresented = false
+    @State private var showingAlert = false
 
     init(itemColor: Color) {
         self.itemColor = itemColor
-        self.items = []
+
+        if let itemIDList = UserDefaults.standard.array(forKey: "setups_\(self.itemColor)") as? [String] {
+            let realm = try! Realm()
+            let cachedItemList = Array(realm.objects(DeskItem.self))
+            var setUpItemList: [DeskItem] = []
+
+            for id in itemIDList {
+                for item in cachedItemList {
+                    if id == item.id {
+                        setUpItemList.append(item)
+                    }
+                }
+            }
+            self.items = setUpItemList
+        } else {
+            self.items = []
+        }
     }
 
     var body: some View {
@@ -22,25 +40,61 @@ struct SetupDetailView: View {
             BackgroundView()
 
             ScrollView {
-                VStack(alignment: .leading, spacing: 16) {
-                    Text("アイテムを組み合わせて、お気に入りのセットアップを作成しよう！")
-                        .foregroundColor(.white)
-
-                    Button(action: { isAddSetupViewPresented = true }) {
-                        Text("セットアップを作成")
-                            .frame(width: UIScreen.main.bounds.width - 32, height: 44)
-                            .foregroundColor(Color.white)
-                            .background(.blue)
+                // アイテムがない時はこちらを表示する
+                if items.isEmpty {
+                    VStack(alignment: .leading, spacing: 16) {
+                        Text("アイテムを組み合わせて、お気に入りのセットアップを作成しよう！")
+                            .foregroundColor(.white)
+                        
+                        Button(action: { isAddSetupViewPresented = true }) {
+                            Text("セットアップを作成")
+                                .frame(width: UIScreen.main.bounds.width - 32, height: 44)
+                                .foregroundColor(Color.white)
+                                .background(.blue)
+                        }
+                        .cornerRadius(8)
+                        .sheet(isPresented: $isAddSetupViewPresented) {
+                            SetupSelectItemView(itemColor: itemColor, setupItems: $items)
+                        }
+                        
+                        Spacer()
                     }
-                    .cornerRadius(8)
-                    .sheet(isPresented: $isAddSetupViewPresented) {
-                        SetupSelectItemView()
+                } else {
+                    VStack {
+                        ForEach(items, id: \.self) { item in
+                            SelectItemLabel(itemName: item.title, itemImage: ImageManager.shared.getImage(name: item.id))
+                        }
                     }
-
-                    Spacer()
                 }
             }
             .padding(16)
+            .toolbar {
+                if !items.isEmpty {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Menu {
+                            Button(
+                                "セットアップを削除",
+                                role: .destructive,
+                                action: { showingAlert = true }
+                            )
+                        } label: {
+                            Image(systemName: "ellipsis.circle")
+                                .roundButton()
+                        }
+                    }
+                }
+            }
+            .alert("このセットアップを削除しますか？", isPresented: $showingAlert) {
+                Button("キャンセル", role: .cancel, action: {})
+
+                Button("削除", role: .destructive) {
+                    // キャッシュと現在表示している画面の更新
+                    UserDefaults.standard.removeObject(forKey: "setups_\(self.itemColor)")
+                    items = []
+                    
+                    HapticFeedbackManager.shared.play(.impact(.soft))
+                }
+            }
         }
     }
 }
--- Astrum/Astrum/MyDesk/SetupView/SetupSelectItemView.swift
+++ Astrum/Astrum/MyDesk/SetupView/SetupSelectItemView.swift
@@ -9,12 +9,18 @@ import RealmSwift
 import SwiftUI
 
 struct SetupSelectItemView: View {
-    private var items: [DeskItem]
-    @State private var selectedValue: Set<DeskItem> = []
+    private let itemColor: Color
+    @Binding private var setupItems: [DeskItem]
 
+    private var items: [DeskItem]
+    @State private var selectedValues: Set<DeskItem> = []
+    
     @Environment(\.dismiss) private var dismiss
 
-    init() {
+    init(itemColor: Color, setupItems: Binding<[DeskItem]>) {
+        self.itemColor = itemColor
+        self._setupItems = setupItems
+        
         let navigationBarAppearance = UINavigationBarAppearance()
         navigationBarAppearance.configureWithDefaultBackground()
 
@@ -33,7 +39,7 @@ struct SetupSelectItemView: View {
     var body: some View {
         NavigationStack {
             VStack(spacing: 8) {
-                List(selection: $selectedValue) {
+                List(selection: $selectedValues) {
                     ForEach(items, id: \.self) { item in
                         SelectItemLabel(itemName: item.title, itemImage: ImageManager.shared.getImage(name: item.id))
                             .listRowInsets(EdgeInsets())
@@ -46,15 +52,27 @@ struct SetupSelectItemView: View {
                 .environment(\.editMode, .constant(.active))
                 
                 Button(action: {
+                    // セットアップごとにキーを設定する
+                    let key = "setups_\(itemColor)"
+                    let newItemIDList: [String] = selectedValues.map { $0.id } // キャッシュに保存するアイテムID用のリスト
+                    let newItemList = Array(selectedValues) // 前の画面を更新する為のアイテムリスト
+
+                    // 選択したnewItemIDListsムのIDを配列に保存する
+                    UserDefaults.standard.set(newItemIDList, forKey: key)
+                    
+                    // 前の画面のセットアップの更新
+                    setupItems = newItemList
+                    
+                    HapticFeedbackManager.shared.play(.impact(.soft))
                     dismiss()
                 }) {
                     Text("セットアップの作成")
                         .frame(width: UIScreen.main.bounds.width - 32, height: 44)
                         .foregroundColor(Color.white)
-                        .background(selectedValue.count == 0 ? .gray : .blue)
+                        .background(selectedValues.count == 0 ? .gray : .blue)
                 }
                 .cornerRadius(8)
-                .disabled(selectedValue.count == 0)
+                .disabled(selectedValues.count == 0)
                 
                 Spacer()
             }
@@ -65,7 +83,9 @@ struct SetupSelectItemView: View {
 }
 
 struct SetupSelectItemView_Previews: PreviewProvider {
+    @State private static var sampleItemList = [DeskItemFixture.sampleItem()]
+
     static var previews: some View {
-        SetupSelectItemView()
+        SetupSelectItemView(itemColor: .orange, setupItems: $sampleItemList)
     }
 }
