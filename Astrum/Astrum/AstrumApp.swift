ef449f214e0275c7bf5617568fef7a66b2b76bfb
mercury0517 <aki19960517@gmail.com>
2023-01-28 16:10:18 +0900

    iOS14以降のATT対応

--- Astrum/Astrum/AstrumApp.swift
+++ Astrum/Astrum/AstrumApp.swift
@@ -5,15 +5,21 @@
 //  Created by 伊原明宏 on 2022/10/22.
 //
 
-import SwiftUI
-
-import SwiftUI
+import AppTrackingTransparency
 import GoogleMobileAds
+import SwiftUI
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+                GADMobileAds.sharedInstance().start(completionHandler: nil)
+            })
+        } else {
+            // Fallback on earlier versions
+            GADMobileAds.sharedInstance().start(completionHandler: nil)
+        }
         return true
     }
 }
