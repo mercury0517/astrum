56dbeba2b4c9a4d7b5e09c6830cb98f6b25908bd
mercury0517 <aki19960517@gmail.com>
2023-01-29 18:38:49 +0900

    ATTダイアログが表示されない問題を修正

--- Astrum/Astrum/AstrumApp.swift
+++ Astrum/Astrum/AstrumApp.swift
@@ -12,14 +12,6 @@ import SwiftUI
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        if #available(iOS 14, *) {
-            ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
-                GADMobileAds.sharedInstance().start(completionHandler: nil)
-            })
-        } else {
-            // Fallback on earlier versions
-            GADMobileAds.sharedInstance().start(completionHandler: nil)
-        }
         return true
     }
 }
@@ -27,9 +19,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 @main
 struct AstrumApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             LaunchScreen()
+                .onAppear {
+                    requestAppTrackingTransparencyAuthorization()
+                }
+        }
+    }
+
+    private func requestAppTrackingTransparencyAuthorization() {
+        if #available(iOS 14.5, *) {
+            guard ATTrackingManager.trackingAuthorizationStatus == .notDetermined else { return }
+            
+            print("1111111")
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+                    print("2222")
+                    if status == .authorized {
+                        print("3333")
+                        GADMobileAds.sharedInstance().start(completionHandler: nil)
+                    }
+                })
+            }
         }
     }
 }
